3722dc3b78c9e9bc56f3f8e0e0c50efbb06de6bb
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-02-17 22:10:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all the js code as newer version//
+
+//alert(3+3)// We are using node js not browser
+
+console.log(3+3)
+console.log("Arsum")
+
+let name = "Arsum" // string
+let age = 21          // number
+let isLoggedin = false    // boolean
+
+console.log(typeof "Arsum");  // string
+console.log(typeof age);  // number
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
