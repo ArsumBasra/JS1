b15f6d7fc2a074f1c4145b35086f229510fab0e9
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-02-22 19:15:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "undefined" //agar value "" men hui to string nhi to number// 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueinnum = Number (score)
+
+console.log(typeof valueinnum);
+console.log(valueinnum);
+
+let isloggedin = "Arsum"
+
+let booleanisloggedin = Boolean (isloggedin)
+
+console.log(booleanisloggedin);
+
+let somenum = 123
+
+let stringnum = String (somenum)
+
+console.log(stringnum);
+
+console.log(typeof stringnum);
+
+
+
+
+
