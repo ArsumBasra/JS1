df9bbd17277a4a609abfffe5f0e9e03025486b87
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-02-22 19:16:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 7818
+let accountemail = "basraarsum@google.com"
+var accountpassword = "Marbaisgone"
+accountcity = "Peshawar"
+let accountstate;
+
+// accountId = 227818 not allowed
+
+accountemail = "hs.hs@gmail.com"
+accountpassword = "tayyababaddie"
+accountcity = "Faisalabad"
+
+console.log(accountId);
+
+/* Prefer not to use var
+Due to issue on block scope and functional scope
+*/
+
+console.table([accountId, accountemail, accountpassword, accountcity, accountstate])
+
