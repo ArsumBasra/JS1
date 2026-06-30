6c84253d1022dc222eb6c1c2a2837b509d76be87
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-06-30 07:54:11 +0000

    We understood the cincept of strings in JavaScript

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,58 @@
+const name = "Marva"
+const repo = 24
+
+// console.log(name + "Arsum" + repo);
+
+console.log(`Her name is ${name} and she was ${repo} `);
+
+const Marva = new String ("Arsum-ab-Marva")
+
+console.log(Marva [0]);
+
+console.log(Marva.__proto__);
+
+console.log(Marva.length);
+
+console.log(Marva.charAt(4));
+
+console.log(Marva.indexOf('A'));
+
+console.log(Marva.toUpperCase());
+
+const NewString = Marva.substring(0, 4)
+
+console.log(NewString);
+
+const anotherstring = Marva.slice(-6, 4)
+
+console.log(anotherstring);
+
+const Arsum = "                    Marva                     Salim "
+
+console.log(Arsum)
+
+console.log(Arsum.trim());
+
+const url = "https://hitesh.com/hitesh%20chalchalchal"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('chal'))
+
+console.log(Marva.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
