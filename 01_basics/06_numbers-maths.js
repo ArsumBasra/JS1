03479d664c8be89cfdeced776a1f2a0e958f86c3
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-07-07 19:27:09 +0000

    Understand the concept of Maths and numbers in JavaScript

---
+++ 01_basics/06_numbers-maths.js
@@ -0,0 +1,56 @@
+const Tayyaba = 100
+
+console.log(Tayyaba);
+
+const Marva = new Number(200)
+console.log(Marva);
+
+console.log(Marva.toString().length);
+
+console.log(Marva.toFixed(2));
+
+const Iqra = 24.97869
+
+console.log(Iqra.toPrecision(3))
+
+const Aqsa = 1000000000000000000000
+
+console.log(Aqsa.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++Maths++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(1.2));
+console.log(Math.floor(1.9));
+console.log(Math.min(1, 7, 0, 9));
+console.log(Math.max(2, 4, 6, 7));
+
+console.log(Math.random());
+console.log((Math.random()* 10) + 1);
+console.log(Math.floor(Math.random()* 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max- min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
