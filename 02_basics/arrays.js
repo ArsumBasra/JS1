c0cb76e6bb7ebd17bbfcb80387d854e9f6312eff
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-07-31 21:36:22 +0000

    Arrays in JavaScript

---
+++ 02_basics/arrays.js
@@ -0,0 +1,49 @@
+// Arrays
+
+const myarray = [0, 1, 2, 3, 4, 5]
+
+// const peteservants = ["Choto", "Geenu", "Mithu", "Mozart"]
+
+// const myarray2 = new Array(1, 2, 3, 4)
+
+// console.log(myarray[0]);
+
+// myarray.push(6)
+// myarray.pop()
+// myarray.unshift(9)
+// myarray.shift()
+// console.log(myarray.includes(9));
+// console.log(myarray.indexOf(3));
+
+const newarr = myarray.join()
+
+console.log(myarray);
+
+
+console.log(myarray);
+
+console.log(typeof myarray);
+
+// slice splice
+
+console.log("A ", myarray);
+const Marva = myarray.slice(1, 3)
+
+console.log(Marva);
+
+console.log("B ", myarray);
+
+const Tayyaba = myarray.splice(1, 3)
+
+console.log(Tayyaba);
+
+console.log("C ", myarray);
+
+
+
+
+
+
+
+
+
