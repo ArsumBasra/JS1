ae6c72fb4a0ba254060ff9abb1ee6cbfa2c1fbde
Arsum Basra <154627270+ArsumBasra@users.noreply.github.com>
2026-08-03 15:40:26 +0000

    Modified the file

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -39,6 +39,47 @@ console.log(Tayyaba);
 
 console.log("C ", myarray);
 
+const marvel = ["Spiderman", "Daredevil", "MoonKnight"]
+
+const DC = ["Batman", "Superman", "WonderWoman"]
+
+// marvel.push(DC)
+
+// console.log(marvel); 
+
+// console.log(marvel[3][2]);
+
+const heroes = marvel.concat(DC)
+console.log(heroes);
+
+const heroesagain = [...marvel, ...DC]
+
+console.log(heroesagain);
+
+const anotherarray = [1, 2, 3, [3, 5, 6, [7, 8, 9, [0, 8]]]]
+const anotherarray1 = anotherarray.flat(Infinity)
+console.log(anotherarray1);
+
+console.log(Array.isArray("Iqra"));
+
+console.log(Array.from("Iqra"));
+
+console.log(Array.from({name: "Arsum"}));
+
+let score1 = 1000
+let score2 = 2000
+let score3 = 30000
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
+
+
+
+
 
 
 
